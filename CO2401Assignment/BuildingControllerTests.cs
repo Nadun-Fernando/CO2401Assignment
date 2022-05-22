ced20aa7f721da3f47f25e44847bd67c1a24257a
Nadun Fernando <nadunnilupul@gmail.com>
2022-05-23 04:06:28 +0530

    initial commit with classes and interfaces

---
+++ CO2401Assignment/BuildingControllerTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace CO2401Assignment;
+
+public class BuildingControllerTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
