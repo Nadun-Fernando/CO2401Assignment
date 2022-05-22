0e54f14fd927d6d2ca80f0ee2b4f528f4cb655ac
Nadun Fernando <nadunnilupul@gmail.com>
2022-05-23 04:27:01 +0530

    Added Level 2 Requirements

--- CO2401Assignment/BuildingController.cs
+++ CO2401Assignment/BuildingController.cs
@@ -1,4 +1,6 @@
-﻿namespace CO2401Assignment;
+﻿using System;
+
+namespace CO2401Assignment;
 
 public class BuildingController
 {
@@ -9,6 +11,22 @@ public class BuildingController
     {
         buildingID = id.ToLower();
         currentState = "out of hours";
+    }
+
+    public BuildingController(string buildingId, string startState)
+    {
+        buildingID = buildingId.ToLower();
+        
+        if (startState is "closed" or "open" or "out of hours")
+        {
+            currentState = startState.ToLower();
+        }
+        else
+        {
+            throw new ArgumentException(
+                "Argument Exception: BuildingController can only be initialised to the following states 'open', 'closed', 'out of hours'");
+        }
+        
     }
 
     public string GetBuildingID()
@@ -28,7 +46,7 @@ public class BuildingController
 
     public bool SetCurrentState(string state)
     {
-        string historyState;
+        string historyState = "";
 
         if (currentState == "closed" && state is "out of hours")
         {
@@ -42,6 +60,16 @@ public class BuildingController
         {
             currentState = state;
         }
+        else if (currentState is "closed" or "out of hours" or "open" && state is "fire drill" or "fire alarm")
+        {
+            historyState = currentState;
+            currentState = state;
+        }
+        else if (currentState is "fire drill" or "fire alarm" && state is "closed" or "out of hours" or "open" )
+        {
+            currentState = historyState;
+            SetCurrentState(state);
+        }
         
         /*if (state is "closed" or "out of hours" or "open" or "fire drill" or "fire alarm")
         {
@@ -49,6 +77,6 @@ public class BuildingController
             return true;
         }*/
 
-        return false;
+        return true;
     }
 }
