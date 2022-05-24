20426d8fdae788db0d97fe464c5e5090bb2c0c53
NumeralArcher <lehande28@gmail.com>
2022-05-24 22:00:52 +0530

    test case 1

--- CO2401Assignment/BuildingControllerTests.cs
+++ CO2401Assignment/BuildingControllerTests.cs
@@ -1,17 +1,45 @@
+using NSubstitute;
 using NUnit.Framework;
 
 namespace CO2401Assignment;
 
+[TestFixture]
 public class BuildingControllerTests
 {
+
+    private IDoorManager _doorManager;
+    private IEmailService _emailService;
+    private IFireAlarmManager _fireAlarmManager;
+    private ILightManager _lightManager;
+    private IWebService _webService;
+
+    private BuildingController _buildingController;
+
     [SetUp]
     public void Setup()
     {
+        _doorManager = Substitute.For<IDoorManager>();
+        _emailService = Substitute.For<IEmailService>();
+        _lightManager = Substitute.For<ILightManager>();
+        _fireAlarmManager = Substitute.For<IFireAlarmManager>();
+        _webService = Substitute.For<IWebService>();
+
+        
+
     }
 
     [Test]
-    public void Test1()
+    public void Check_if_ID_passed_to_through_Constructor()
     {
-        Assert.Pass();
+        //Arrange
+        var iD = "sdfkjljkl";
+        _buildingController = new BuildingController(iD);
+        
+        //Act
+        var result1 = _buildingController.GetBuildingID();
+
+        //Assert
+        Assert.AreEqual(result1, iD);
+
     }
 }
